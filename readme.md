e2bd9e33f199540e7e2340f6ef8389f9e6aedec9
david <106287449+davids691@users.noreply.github.com>
2024-01-08 14:17:21 -0700

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,4 @@
+have python installed and pip installed
+
+python code_gui.py
+
---
+++ script.js
@@ -0,0 +1,159 @@
+// personality calls \\
+const citra = document.getElementById("citra");
+const rowan = document.getElementById("rowan");
+const sc = document.getElementById("sc");
+const sf = document.getElementById("sf");
+const sg = document.getElementById("sg");
+const esme = document.getElementById("esme");
+// quote calls \\
+const citra_quote = document.getElementById("citra_quote");
+const rowan_quote = document.getElementById("rowan_quote");
+const sc_quote = document.getElementById("sc_quote");
+const sf_quote = document.getElementById("sf_quote");
+const sg_quote = document.getElementById("sg_quote");
+const sp_quote = document.getElementById("sp_quote");
+const sv_quote = document.getElementById("sv_quote");
+// journal calls \\
+const citra_jounal = document.getElementById("citra_jounal");
+const rowan_jounal = document.getElementById("rowan_jounal");
+const sc_jounal = document.getElementById("sc_jounal");
+const sf_jounal = document.getElementById("sf_jounal");
+const sg_jounal = document.getElementById("sg_jounal");
+const sp_jounal = document.getElementById("sp_jounal");
+const sv_jounal = document.getElementById("sv_jounal");
+
+
+
+
+
+// personality scripts \\
+
+
+  // citra \\
+
+citra.addEventListener("click", function() {
+  window.open("citra.html", "_self");
+  });
+
+  // rowan \\
+
+rowan.addEventListener("click", function() {
+  window.open("rowan.html", "_self");
+  });
+
+  // scythe curie \\
+
+sc.addEventListener("click", function() {
+  window.open("scythe-curie.html", "_self");
+  });
+
+  // scythe faraday \\
+
+sf.addEventListener("click", function() {
+  window.open("scythe-faraday.html", "_self");
+  });
+
+  // scythe goddard \\
+
+sg.addEventListener("click", function() {
+  window.open("scythe-goddard.html", "_self");
+  });
+
+  // esme \\
+
+esme.addEventListener("click", function() {
+  window.open("esme.html", "_self");
+  });
+
+
+// quote scripts \\
+
+
+  // citra \\
+
+  citra_quote.addEventListener("click", function() {
+    window.open("citra_quote.html", "_self");
+    });
+  
+    // rowan \\
+  
+  rowan_quote.addEventListener("click", function() {
+    window.open("rowan_quote.html", "_self");
+    });
+  
+    // scythe curie \\
+  
+  sc_quote.addEventListener("click", function() {
+    window.open("scythe-curie_quote.html", "_self");
+    });
+  
+    // scythe faraday \\
+  
+  sf_quote.addEventListener("click", function() {
+    window.open("scythe-faraday_quote.html", "_self");
+    });
+  
+    // scythe goddard \\
+  
+  sg_quote.addEventListener("click", function() {
+    window.open("scythe-goddard_quote.html", "_self");
+    });
+
+    // scythe prometheus \\
+  
+  sp_quote.addEventListener("click", function() {
+    window.open("H.S-Prometheus_quote.html", "_self");
+    });
+
+    // scythe volta \\
+  
+  sv_quote.addEventListener("click", function() {
+    window.open("volta_quote.html", "_self");
+    });
+  
+  
+// journal scripts \\
+
+
+  // citra \\
+
+  citra_jounal.addEventListener("click", function() {
+    window.open("citra_journal.html", "_self");
+    });
+  
+    // rowan \\
+  
+  rowan_jounal.addEventListener("click", function() {
+    window.open("rowan_journal.html", "_self");
+    });
+  
+    // scythe curie \\
+  
+  sc_jounal.addEventListener("click", function() {
+    window.open("scythe-curie_journal.html", "_self");
+    });
+  
+    // scythe faraday \\
+  
+  sf_jounal.addEventListener("click", function() {
+    window.open("scythe-faraday_journal.html", "_self");
+    });
+  
+    // scythe goddard \\
+  
+  sg_jounal.addEventListener("click", function() {
+    window.open("scythe-goddard_journal.html", "_self");
+    });
+  
+    // scythe prometheus \\
+  
+    sp_jounal.addEventListener("click", function() {
+      window.open("H.S-Prometheus_journal.html", "_self");
+      });
+
+    // scythe prometheus \\
+  
+    sv_jounal.addEventListener("click", function() {
+      window.open("volta_journal.html", "_self");
+      });
+    
